f7a5e16e5fcac61db7355211a580c515fa7000af
watsonjph <corsegajoseph@gmail.com>
2025-07-09 11:00:59 +0800

    Fix?

---
+++ CSTest/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1st Commit lolz \n");
+Console.WriteLine("Im sleepy but it's okay \n");
+Console.WriteLine("2nd Commit \n");
